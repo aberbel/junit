8251f4d901891ccfc903a83e220dab4e1d1e5632
Setillo <setillofm@gmail.com>
2021-05-21 19:14:17 +0200

    refactor

--- src/test/java/auxiliar/junit/categories/IgnoredTest.java
+++ src/test/java/auxiliar/junit/categories/IgnoredTest.java
@@ -3,10 +3,10 @@ package auxiliar.junit.categories;
 import org.junit.Test;
 import org.junit.Ignore;
 
+@Ignore
 public class IgnoredTest {
 
-	@Test
-	@Ignore
+	@Ignore("Mensaje alternativo con justificación") @Test
 	public void testOne() {
 		System.out.println("IgnoredTest");
 	}
